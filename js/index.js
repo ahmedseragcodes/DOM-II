49ba6dbd9e1a987511b783c932a5ddf495681106
ahmedseragcodes <ahmedserag1@gmail.com>
2020-10-27 17:25:07 -0400

    added preventdefault to eliminate vertical scroll when space is pressed and utilized doubleclick event listener

--- js/index.js
+++ js/index.js
@@ -1,9 +1,23 @@
+//When attempting to drag the home navigation element, for instance to open in a new tab, change text color to red
+
 const headerNav=document.querySelectorAll("header nav a");
 headerNav[0].addEventListener("drag", function(event){
     event.target.style.color="red";
 })
 
+//When double clicking on an image, aqua border is created around it 
+const imgHolder=document.querySelectorAll("img");
+imgHolderArray=Array.from(imgHolder);
+imgHolderArray.forEach(function(item){
+    item.addEventListener("dblclick",function(event){
+    event.target.style.border="1rem solid aqua";
+});
+});
 
+//Preventing document scroll when space bar is pressed 
+document.addEventListener("keydown", function(event){
+    event.preventDefault();
+})
 
 // const btnStorage=document.querySelectorAll(".btn");
 // btnStorage[0].style.backgroundColor="red";

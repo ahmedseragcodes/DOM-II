663010b8a1c2be0fa59c32f8edcc4e18c714649c
ahmedseragcodes <ahmedserag1@gmail.com>
2020-10-27 16:46:40 -0400

    utilized stopPropagation and created event listener for dragging nav element

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-// Your code goes here
+const headerNav=document.querySelectorAll("header nav a");
+headerNav[0].addEventListener("drag", function(event){
+    event.target.style.color="red";
+})
+
+
+
+// const btnStorage=document.querySelectorAll(".btn");
+// btnStorage[0].style.backgroundColor="red";
+
+// btnStorage[0].addEventListener("click", function(event){
+//     console.log(`event passing through ${event.currentTarget.nodeName}`)
+// });
+
+// document.addEventListener("click",function(event){
+//     event.target.style.border="1rem solid black";
+// })
+
+// const containerHolder=document.querySelector(".home");
+// containerHolder.addEventListener("click",function(event){
+//     event.stopPropagation();
+// })
+

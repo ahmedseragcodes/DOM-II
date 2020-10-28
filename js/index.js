d9577d2a95ed6836812098e928da6cf48896e1fd
ahmedseragcodes <ahmedserag1@gmail.com>
2020-10-27 22:33:07 -0400

    was able to add event listeners for 1/2 more events, but others were unsuccessful still

--- js/index.js
+++ js/index.js
@@ -28,19 +28,38 @@ paraHolder[i].addEventListener("mouseover", function(event){
     event.target.style.fontSize="2rem";
 })
 }
-// const btnStorage=document.querySelectorAll(".btn");
-// btnStorage[0].style.backgroundColor="red";
 
-// btnStorage[0].addEventListener("click", function(event){
-//     console.log(`event passing through ${event.currentTarget.nodeName}`)
-// });
+//text being selected turns yellow
+const hTwoHolder=document.querySelectorAll(".container h2");
+hTwoHolder[0].addEventListener("select", function(event){
+    console.log("this has been selected");
+});
 
-// document.addEventListener("click",function(event){
-//     event.target.style.border="1rem solid black";
-// })
+//scrolling increases text size
+const sectionHolder=document.getElementsByClassName("content-section");
+for (let i=0; i<sectionHolder.length; i++){
+sectionHolder[0].addEventListener("scroll",function(event){
+    alert("scroll has occurred");
+});
+};
+//OR
+document.addEventListener("scroll", function(event){
+    console.log("The User Has scrolled on the document");
+})
+//Using resize to change the color of text for a message
+const contentStorage=document.querySelectorAll(".content-section");
 
-// const containerHolder=document.querySelector(".home");
-// containerHolder.addEventListener("click",function(event){
-//     event.stopPropagation();
-// })
+contentStorage[0].addEventListener("resize",function(event){
+    event.target.style.color="red";
+})
+
+//using stopPropagation to prevent main body from being able to become bordered 
+document.addEventListener("click",function(event){
+    event.target.style.border="1rem solid black";
+})
+
+const containerHolder=document.querySelector(".home");
+containerHolder.addEventListener("click",function(event){
+    event.stopPropagation();
+})
 

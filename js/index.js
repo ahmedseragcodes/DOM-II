68201df43db66c458ce48f3205e75dc4b28caddb
ahmedseragcodes <ahmedserag1@gmail.com>
2020-12-01 17:55:13 -0500

    completed wheel

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-//Mouseover
+//Mouseover that bolds front page header nav links 
 
 const frontPageNavLinks=document.querySelectorAll(".main-navigation a");
 frontPageNavLinks.forEach(function(link){
@@ -7,7 +7,7 @@ frontPageNavLinks.forEach(function(link){
     })
 })
 
- //Keydown
+ //Keydown that takes you to the top of the page when you hit enter 
 
 document.addEventListener("keydown", function(event){
         if (event.key==="Enter"){
@@ -15,5 +15,12 @@ document.addEventListener("keydown", function(event){
         }
     })
 
+//Wheel changing front page main body content color 
+const frontPageMainContent=document.querySelectorAll(".text-content");
+frontPageMainContent.forEach(function(textBlurb){
+    textBlurb.addEventListener("wheel",function(event){
+        event.target.style.color="brown";
+    })
+})
 
 

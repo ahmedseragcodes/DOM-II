1f2a76fffb7318df55c7efccedafb90dc6b8748d
ahmedseragcodes <ahmedserag1@gmail.com>
2020-12-01 16:53:12 -0500

    file setup and have one event down

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-// Your code goes here
+//Mouseover
+
+const frontPageNavLinks=document.querySelectorAll(".main-navigation a");
+frontPageNavLinks.forEach(function(link){
+    link.addEventListener("mouseover", function(event){
+        event.target.style.fontWeight="bold";
+    })
+})
+
+ //Keydown
+
+const frontPageImages=document.querySelectorAll("img");
+frontPageImages.forEach(function(image){
+    image.addEventListener("keydown", function(event){
+        if (event.key===13){
+            console.log("hello");
+        }
+    })
+})
+
+

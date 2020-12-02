014adb2d0edc65dd53a36047808f1a883fd0e7dc
ahmedseragcodes <ahmedserag1@gmail.com>
2020-12-01 22:41:21 -0500

    created subscription form at bottom of front page for deals to get focus event listener working

--- js/index.js
+++ js/index.js
@@ -23,4 +23,47 @@ frontPageMainContent.forEach(function(textBlurb){
     })
 })
 
+//Load console logs successful image load
 
+const frontPageImages=document.querySelectorAll("img");
+frontPageImages.forEach(function(image){
+    image.addEventListener("load", function(event){
+        console.log("successful image load");
+    })
+})
+
+//Creating Form For Subscribers to Use Certain Events / Event Listeners 
+
+//Creation
+const frontPageFooter=document.querySelector(".footer");
+const footerSubForm=document.createElement("form");
+const footerSubFormEmail=document.createElement("input");
+footerSubFormEmail.type="email";
+footerSubFormEmail.name="email";
+footerSubFormEmail.placeholder="Enter Your Email";
+const footerSubFormLabel=document.createElement("label");
+footerSubFormLabel.for="email";
+footerSubFormLabel.textContent="Enter Your Email For The Latest Deals:";
+
+//Styling
+footerSubForm.style.boxSizing="border-box";
+footerSubFormEmail.style.padding="1rem";
+footerSubForm.style.display="flex";
+footerSubForm.style.justifyContent="center";
+footerSubFormEmail.style.border="1rem ridge aqua";
+footerSubFormLabel.style.margin="1rem";
+footerSubFormLabel.style.display="flex";
+footerSubFormLabel.style.justifyContent="center";
+footerSubFormLabel.style.alignItems="center";
+footerSubFormLabel.style.fontSize="1.8rem";
+
+//Appending
+frontPageFooter.append(footerSubForm);
+footerSubForm.append(footerSubFormEmail);
+footerSubForm.prepend(footerSubFormLabel);
+
+
+//Footer expands email form upon focus 
+footerSubFormEmail.addEventListener("focus", function(event){
+    event.target.style.padding="2rem";
+})

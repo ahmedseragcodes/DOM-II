2311156c9524d60888c2a52692c0ab5d1dea20e6
ahmedseragcodes <ahmedserag1@gmail.com>
2021-01-12 16:14:23 -0500

    created 4 event listeners successfully, still working on completing fifth / fixing drop

--- js/index.js
+++ js/index.js
@@ -1,1 +1,46 @@
-// Your code goes here
+// Stop Propagation + click event 
+
+const allElementsArray=Array.from(document.all);
+
+allElementsArray.forEach(function(element){
+    element.addEventListener("click", function(event){
+        console.log("Target", event.target);
+        console.log("Current Target", event.currentTarget);
+        event.stopPropagation();
+    })
+})
+
+//Drag Drop, DROP INCOMPLETE
+
+const frontPageImgsArray=Array.from(document.querySelectorAll("img"));
+
+frontPageImgsArray.forEach(function(img){
+    img.addEventListener("drag", function(event){
+        console.log("target has been dragged");
+    })
+})
+
+frontPageImgsArray.forEach(function(img){
+    img.addEventListener("drop", function(event){
+        console.log("target has been dropped");
+    })
+})
+
+//dblclick on images adds border 
+
+frontPageImgsArray.forEach(function(img){
+    img.addEventListener("dblclick", function(event){
+        event.target.style.border="1rem ridge black";
+    })
+})
+
+//escape key removes borders from images
+
+window.addEventListener("keydown", function(event){
+    if (event.key==="Escape"){
+        console.log("hello");
+        frontPageImgsArray.forEach(function(img){
+            img.style.border="none";
+        })
+    }
+})
